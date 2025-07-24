6d4fe0263b3772ccddb7f9ba36e3d6f6539ab721
Janet <jane824691@gmail.com>
2025-07-24 12:19:21 +0800

    comment: tailwind & img

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <h1 class="text-2xl font-bold mb-6 text-center">Todo List</h1>
+  <!-- 漸層從左上 → 右下bg-gradient-to-r from-green-300從綠 via-blue-500中間藍 to-purple-600到紫 -->
+  <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-300 via-blue-500 to-purple-600">
+    <!-- sm:w-auto是RWD寫法, sm以上會生效w-auto, sm以下是w-full -->
+    <!-- shadow-lg帶陰影, rounded-t-lg 可指定只有上方左右兩邊為圓角 -->
+    <!-- 圖片也可這樣寫bg-[url('https://...')] bg-no-repeat bg-center bg-contain 但沒有背景圖半透明-->
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full sm:w-auto max-w-md custom-bg-img">
+      <!-- 文字漸層色, 使用 bg-clip-text 表示將背景色切成符合文字 -->
+      <!-- 再使用 text-transparent 表示將文字設成透明色 -->
+      <h1 class="text-2xl font-bold mb-6 text-center bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent">Todo List</h1>
       <div class="flex mb-4">
         <!-- @keyup.enter語法糖:
-        原始寫法<input @keyup="handleKeyup">
+        原始寫法
+        <input @keyup="handleKeyup">
 
         在 methods 裡判斷
         methods: {
@@ -17,7 +24,7 @@
         <input 
           v-model="newTodo"
           @keyup.enter="addTodo"
-          class="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          class="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
           placeholder="Add a new todo"
         />
         <button 
@@ -26,6 +33,9 @@
         >
           Add
         </button>
+
+        <!-- 更客製化自定義class可寫在style.css跟tailwind.config.js -->
+        <!-- <button class="btn-1">自訂按鈕</button> -->
       </div>
       <ul>
         <li 
