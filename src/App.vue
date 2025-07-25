7b3e65e9347fd8f690bb95991eaf093f9af10dce
Janet <jane824691@gmail.com>
2025-07-25 13:55:49 +0800

    style: add fontAwesome Icon

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@
     <div class="bg-white p-8 rounded-lg shadow-lg w-full sm:w-auto max-w-md custom-bg-img">
       <!-- 文字漸層色, 使用 bg-clip-text 表示將背景色切成符合文字 -->
       <!-- 再使用 text-transparent 表示將文字設成透明色 -->
-      <h1 class="text-2xl font-bold mb-6 text-center bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent">Todo List</h1>
+      <h1 class="text-2xl font-bold mb-6 text-center bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent">Todo List
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="w-6 h-6 inline-block mr-2 text-blue-500"><path fill="currentColor"
+          d="M320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM288 224C288 206.3 302.3 192 320 192C337.7 192 352 206.3 352 224C352 241.7 337.7 256 320 256C302.3 256 288 241.7 288 224zM280 288L328 288C341.3 288 352 298.7 352 312L352 400L360 400C373.3 400 384 410.7 384 424C384 437.3 373.3 448 360 448L280 448C266.7 448 256 437.3 256 424C256 410.7 266.7 400 280 400L304 400L304 336L280 336C266.7 336 256 325.3 256 312C256 298.7 266.7 288 280 288z"/></svg>
+      </h1>
       <div class="flex mb-4">
         <!-- @keyup.enter語法糖:
         原始寫法
@@ -24,7 +27,7 @@
         <input 
           v-model="newTodo"
           @keyup.enter="addTodo"
-          class="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
+          class="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-300"
           placeholder="Add a new todo"
         />
         <button 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+// 正規需npm i fontawesome以下套件
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// library.add(faCircleInfo)
+
+// const app = createApp(App)
+// app.component('FontAwesomeIcon', FontAwesomeIcon)
+// app.mount('#app')
 
 createApp(App).mount('#app')
