2d692505a8c06e18e0fd8385bbaa31859b4d6dce
Janet <jane824691@gmail.com>
2025-08-16 21:23:08 +0800

    style: width

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <!-- 漸層從左上 → 右下bg-gradient-to-r from-green-300從綠 via-blue-500中間藍 to-purple-600到紫 -->
   <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-300 via-blue-500 to-purple-600">
-    <!-- sm:w-auto是RWD寫法, sm以上會生效w-auto, sm以下是w-full -->
     <!-- shadow-lg帶陰影, rounded-t-lg 可指定只有上方左右兩邊為圓角 -->
     <!-- 圖片也可這樣寫bg-[url('https://...')] bg-no-repeat bg-center bg-contain 但沒有背景圖半透明-->
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full sm:w-auto max-w-md custom-bg-img">
+    <div class="bg-white p-8 rounded-lg shadow-lg cards-width custom-bg-img">
       <!-- 文字漸層色, 使用 bg-clip-text 表示將背景色切成符合文字 -->
       <!-- 再使用 text-transparent 表示將文字設成透明色 -->
       <h1 class="text-2xl font-bold mb-6 text-center bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent">Todo List
@@ -29,6 +28,7 @@
           @keyup.enter="addTodo"
           class="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-300"
           placeholder="Add a new todo"
+          name="Add a new todo"
         />
         <button 
           @click="addTodo"
@@ -47,7 +47,7 @@
           class="flex items-center justify-between p-2 border-b"
         >
       <!-- {{ todo }} 會等於 { "Text": "2222", "completed": false } -->
-          <span :class="{ 'line-through text-gray-500': todo.completed }">{{ todo.text }}</span>
+          <span class="pr-4" :class="{ 'line-through text-gray-500': todo.completed }">{{ todo.text }}</span>
           <div>
             <button 
               @click="toggleTodo(index)"
